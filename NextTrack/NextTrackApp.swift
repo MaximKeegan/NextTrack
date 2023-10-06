ed7298baf475b3ee3c3193945e0855443644e9d3
Maxim Keegan <maxim@keegan.ru>
2023-10-06 19:15:26 +0500

    Initial Commit

---
+++ NextTrack/NextTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextTrackApp.swift
+//  NextTrack
+//
+//  Created by Maxim Keegan on 06.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct NextTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
